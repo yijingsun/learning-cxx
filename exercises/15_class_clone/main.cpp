402c3173cd9d889308c18b318973a633ba40cbf3
Joy <869780236@qq.com>
2025-01-19 01:43:10 +0800

    [UPDATE]提交10题

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -7,20 +7,31 @@
 class DynFibonacci {
     size_t *cache;
     int cached;
+    int capacity_; // 在类中存储数组大小，并在复制构造器中使用
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2), capacity_(capacity) {
+        cache[0]=0;
+        cache[1]=1;
+        cache[2]=1;
+    }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    DynFibonacci(DynFibonacci const & a) : cache(new size_t[a.capacity_]),cached(a.cached),capacity_(a.capacity_) {
+        // 深拷贝
+        std::copy(a.cache, a.cache+capacity_, cache);
+    };
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci() {
+        delete[] cache;
+        cache = nullptr;
+    };
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; cached<i+1; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
--- exercises/18_class_virtual/main.cpp
+++ exercises/18_class_virtual/main.cpp
@@ -3,7 +3,7 @@
 // READ: 虚函数 <https://zh.cppreference.com/w/cpp/language/virtual>
 
 struct A {
-    virtual char virtual_name() const {
+    virtual char virtual_name() const { // 虚函数
         return 'A';
     }
     char direct_name() const {
@@ -12,7 +12,8 @@ struct A {
 };
 struct B : public A {
     // READ: override <https://zh.cppreference.com/w/cpp/language/override>
-    char virtual_name() const override {
+    char virtual_name() const override { // B::virtual_name覆盖A::virtual_name，函数名相同，无需声明override 
+    // override 说明符确保该函数为虚函数并覆盖某个基类中的虚函数
         return 'B';
     }
     char direct_name() const {
@@ -21,14 +22,15 @@ struct B : public A {
 };
 struct C : public B {
     // READ: final <https://zh.cppreference.com/w/cpp/language/final>
-    char virtual_name() const final {
+    char virtual_name() const final { // C::virtual_name覆盖A::virtual_name
+    //final 说明符确保函数为虚并指定它不能被派生类覆盖，所以C::virtual_name不能被覆盖
         return 'C';
     }
     char direct_name() const {
         return 'C';
     }
 };
-struct D : public C {
+struct D : public C { // D::virtual_name=C::virtual_name
     char direct_name() const {
         return 'D';
     }
@@ -42,38 +44,38 @@ int main(int argc, char **argv) {
     C c;
     D d;
 
-    ASSERT(a.virtual_name() == '?', MSG);
-    ASSERT(b.virtual_name() == '?', MSG);
-    ASSERT(c.virtual_name() == '?', MSG);
-    ASSERT(d.virtual_name() == '?', MSG);
-    ASSERT(a.direct_name() == '?', MSG);
-    ASSERT(b.direct_name() == '?', MSG);
-    ASSERT(c.direct_name() == '?', MSG);
-    ASSERT(d.direct_name() == '?', MSG);
+    ASSERT(a.virtual_name() == 'A', MSG);
+    ASSERT(b.virtual_name() == 'B', MSG);
+    ASSERT(c.virtual_name() == 'C', MSG);
+    ASSERT(d.virtual_name() == 'C', MSG);
+    ASSERT(a.direct_name() == 'A', MSG);
+    ASSERT(b.direct_name() == 'B', MSG);
+    ASSERT(c.direct_name() == 'C', MSG);
+    ASSERT(d.direct_name() == 'D', MSG);
 
     A &rab = b;
     B &rbc = c;
     C &rcd = d;
 
-    ASSERT(rab.virtual_name() == '?', MSG);
-    ASSERT(rbc.virtual_name() == '?', MSG);
-    ASSERT(rcd.virtual_name() == '?', MSG);
-    ASSERT(rab.direct_name() == '?', MSG);
-    ASSERT(rbc.direct_name() == '?', MSG);
-    ASSERT(rcd.direct_name() == '?', MSG);
+    ASSERT(rab.virtual_name() == 'B', MSG);
+    ASSERT(rbc.virtual_name() == 'C', MSG);
+    ASSERT(rcd.virtual_name() == 'C', MSG);
+    ASSERT(rab.direct_name() == 'A', MSG);
+    ASSERT(rbc.direct_name() == 'B', MSG);
+    ASSERT(rcd.direct_name() == 'C', MSG);
 
     A &rac = c;
     B &rbd = d;
 
-    ASSERT(rac.virtual_name() == '?', MSG);
-    ASSERT(rbd.virtual_name() == '?', MSG);
-    ASSERT(rac.direct_name() == '?', MSG);
-    ASSERT(rbd.direct_name() == '?', MSG);
+    ASSERT(rac.virtual_name() == 'C', MSG);
+    ASSERT(rbd.virtual_name() == 'C', MSG);
+    ASSERT(rac.direct_name() == 'A', MSG);
+    ASSERT(rbd.direct_name() == 'B', MSG);
 
     A &rad = d;
 
-    ASSERT(rad.virtual_name() == '?', MSG);
-    ASSERT(rad.direct_name() == '?', MSG);
+    ASSERT(rad.virtual_name() == 'C', MSG);
+    ASSERT(rad.direct_name() == 'A', MSG);
 
     return 0;
 }
