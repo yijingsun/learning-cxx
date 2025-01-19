70d13249d599879f96b198ae051fba1bf0e50d39
Joy <869780236@qq.com>
2025-01-20 03:35:45 +0800

    [FIX]修改一些错误

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include "../exercise.h"
+#include <cstring>
 
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
@@ -20,7 +21,7 @@ struct Tensor4D {
         }
         
         data = new T[size];
-        std::memcpy(data, data_, size * sizeof(T));
+        memcpy(data, data_, size * sizeof(T));
     }
     ~Tensor4D() {
         delete[] data;
